d0b4f2e3fbce2f4a4f6ee10bc1f24e03bfca4188
Scott Pantall <aspantall@gmail.com>
2017-05-09 16:22:02 -0600

    init commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+/**
+ *
+ * @author Scott Pantall
+ */
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        Path file = Paths.get(args[0]);
+        BufferedReader buffer = Files.newBufferedReader(file);
+        String line;
+        
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+            //System.out.println(line);
+            intersection(line);
+        }
+    }
+    
+    public static void intersection(String line) {
+        String[] lists = line.split(":");
+        
+        
+        /*
+        ArrayList<String> uniques = new ArrayList<>();
+        
+        for(int i = 0; i < numbers.length; i++) {
+            if(!uniques.contains(numbers[i])) {
+                uniques.add(numbers[i]);
+            }
+        }
+        
+        for(String s : uniques) {
+            System.out.print(s);
+            if(uniques.indexOf(s) != uniques.size() - 1) {
+                System.out.print(",");
+            }
+            else {
+                System.out.printf("%n");
+            }
+        }
+                */
+    }
+}
